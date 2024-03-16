cdf4b736e83b799c7ce8d8d1100e333e69f599a8
sunnyheee <fool0116@naver.com>
2024-03-16 16:18:18 +0900

    style in sidebat

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -9,13 +9,19 @@ type Props = {
 const Sidebar = ({ user: { name, username, image } }: Props) => {
   return (
     <>
-      <div>
+      <div className="flex items-center">
         {image && <Avatar image={image} />}
-        <p>{username}</p>
-        <p>{name}</p>
+        <div className="ml-4">
+          <p className="font-bold">{username}</p>
+          <p className="text-lg text-neutral-500 leading-4">{name}</p>
+        </div>
       </div>
-      <p>About / Help / Press / API / Jobs </p>
-      <p>@Copyright SNS from METAL</p>
+      <p className="text-sm text-neutral-500 mt-8">
+        About / Help / Press / API / Jobs{" "}
+      </p>
+      <p className="font-bold text-sm mt-8 text-neutral-500">
+        @Copyright SNS from METAL
+      </p>
     </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,21 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-  console.log(session);
   const user = session?.user;
-  console.log(user, "user!!");
 
   if (!user) {
     redirect("/auth/signin");
   }
 
   return (
-    <section>
-      <FollowingBar />
-      <PostList />
-      <Sidebar user={user} />
+    <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className=" basis-1/4">
+        <Sidebar user={user} />
+      </div>
     </section>
   );
 }
