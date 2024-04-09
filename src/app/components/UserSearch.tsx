f7544199e5c43740af787c886671541e727e2ca5
sunnyheee <fool0116@naver.com>
2024-04-10 00:18:22 +0900

    search

---
+++ src/app/components/UserSearch.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { FormEvent, useState } from "react";
+import useSWR from "swr";
+import GridSpinner from "./ui/GridSpinner";
+import { ProfileUser } from "@/model/user";
+import UserCard from "./UserCard";
+
+export default function UserSearch() {
+  const [keyword, setKeyword] = useState("");
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center">
+      <form onSubmit={onSubmit} className="w-full mb-4">
+        <input
+          className="w-full text-xl p-3 outline-none border border-gray-400"
+          type="text"
+          autoFocus
+          placeholder="Search for a username or name"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+      {error && <p>エラーが発生しました。</p>}
+      {isLoading && <GridSpinner />}
+      {!isLoading && !error && users?.length === 0 && (
+        <p>お探しのユーザーがないです。</p>
+      )}
+      <ul className="w-full p-4">
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <UserCard user={user} />
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+}
--- src/service/user.ts
+++ src/service/user.ts
@@ -1,3 +1,4 @@
+import { ProfileUser } from "@/model/user";
 import { client } from "./sanity";
 
 type OAuthUser = {
@@ -30,4 +31,25 @@ export async function getUserByUsername(username: string) {
       "bookmarks":bookmarks[]->_id
     }`
   );
+}
+
+export async function searchUsers(keyword?: string) {
+  const query = keyword
+    ? `&& (name match "${keyword}") || (username match "${keyword}")`
+    : "";
+  return client.fetch(
+    `*[_type =="user" ${query}]{
+      ...,
+      "following": count(following),
+      "followers": count(followers),
+    }
+    `
+  );
+  // .then((users) =>
+  //   users.map((user: ProfileUser) => ({
+  //     ...user,
+  //     following: user.following ?? 0,
+  //     followers: user.followers ?? 0,
+  //   }))
+  // );
 }
